64360d2f58933624a9923553c7b40d323b11afc4
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2020-11-15 10:01:05 -0800

    Update index.js
    trying to stop from hanging

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -3,4 +3,6 @@ var server = http.createServer(function (req, res) {
 res.writeHead(200);
 res.end("Hello world!");
 });
-server.listen(3000);
+server.listen(3000, ()=>{
+   console.log("server started at port "+3000);
+});
